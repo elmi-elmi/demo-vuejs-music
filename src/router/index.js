3b8412da5dbbde88cbf86ea2dd74ae647faf72ca
elmi-elmi <shahrokhelmi@gmail.com>
2021-11-26 18:49:53 +0330

    nProgress

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '@/views/Home.vue';
-import About from '@/views/About.vue';
-import Manage from '@/views/Manage.vue';
+
 import store from '@/store'; // ../store/index
-import Song from '@/views/Song.vue';
+
+const Home = () => import('@/views/Home.vue');
+const About = () => import('@/views/About.vue');
+const Manage = () => import(/* webpackChunkName:"groupedChunk" */'@/views/Manage.vue');
+const Song = () => import(/* webpackChunkName:"groupedChunk" */'@/views/Song.vue');
 
 const routes = [
   {
